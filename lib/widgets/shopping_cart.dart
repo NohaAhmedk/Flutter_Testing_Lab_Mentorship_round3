20199d6fbab4e30548fedcc5ee60097d4e1e5f00
nohaahmed <nohaahmedk1@gmail.com>
2025-10-16 18:36:53 +0300

    feat: Refactor shopping cart logic
    - Update `addItem` to increment quantity if an item already exists in the cart.
    - Correct the total discount calculation to be based on the item's price.
    - Fix the `totalAmount` calculation to subtract the discount from the subtotal.

--- lib/widgets/shopping_cart.dart
+++ lib/widgets/shopping_cart.dart
@@ -28,9 +28,14 @@ class _ShoppingCartState extends State<ShoppingCart> {
 
   void addItem(String id, String name, double price, {double discount = 0.0}) {
     setState(() {
-      _items.add(
-        CartItem(id: id, name: name, price: price, discount: discount),
-      );
+      final index = _items.indexWhere((item) => item.id == id);
+      if (index != -1) {
+        _items[index].quantity += 1;
+      } else {
+        _items.add(
+          CartItem(id: id, name: name, price: price, discount: discount),
+        );
+      }
     });
   }
 
@@ -70,13 +75,13 @@ class _ShoppingCartState extends State<ShoppingCart> {
   double get totalDiscount {
     double discount = 0;
     for (var item in _items) {
-      discount += item.discount * item.quantity;
+      discount += (item.price * item.discount) * item.quantity;
     }
     return discount;
   }
 
   double get totalAmount {
-    return subtotal + totalDiscount;
+    return subtotal - totalDiscount;
   }
 
   int get totalItems {
